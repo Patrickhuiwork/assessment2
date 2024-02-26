cf2f0d08612b12875c9340511e4d6dd421e0cdd8
Patrickhuiwork <patrick.hui.work@gmail.com>
2024-02-25 16:46:51 -0800

    setup font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import "@/styles/globals.css";
 import Header from "@/components/Header";
+import Head from "next/head";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
   <main>
+  <Head>
+  <link href="https://api.fontshare.com/v2/css?f[]=satoshi@1,2&display=swap" rel="stylesheet"/>
+  </Head>
   <Header/>
   <Component {...pageProps} />;
   </main>
