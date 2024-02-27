d5e233f5c35df214e27bc8fa6bc1b7a66a85d76b
Patrickhuiwork <patrick.hui.work@gmail.com>
2024-02-26 19:08:55 -0800

    News Letter
    only need to add spline animations

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -8,6 +8,15 @@ export default function About() {
             <h1 className={classes.title}>About Us</h1>
             <p className={classes.paragraph}>MoodBrew was created in order to foster a sense of well-being and mindfulness in our community. Through a meticulous selection process, we source the finest organic ingredients to create blends that not only tantalize the taste buds but also nurture the body and soul.</p>
             <p className={classes.paragraph}>Join us on this journey towards wellness, one cup at a time.</p>
+            <div className={classes.newsletter}>
+                <div className={classes.box}>
+                    <h3>Join Our News Letter</h3>
+                    <form>
+                        <input type="email" placeholder="Email" className={classes.email}/>
+                        <button type="submit" className={classes.submit}>Get Notified</button>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
   )
